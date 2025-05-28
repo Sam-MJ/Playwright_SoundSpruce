1b3e72770743bf74573e17dc1581f29cf4e9066b
Sam-MJ <tf2redspark@gmail.com>
2025-05-28 19:17:55 +0100

    Created Log In Page Model and basic sign in form test with user secrets

---
+++ LoginPageFormTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Playwright;
+using Microsoft.Playwright.Xunit;
+using Playwright_SoundSpruce.Models;
+
+namespace Playwright_SoundSpruce
+{
+    public class LoginPageFixture
+    {
+        public readonly string TestUserName;
+        public readonly string TestPassword;
+        public LoginPageFixture()
+        {
+            var builder = new ConfigurationBuilder().AddUserSecrets(Assembly.GetExecutingAssembly(), false);
+            IConfiguration config = builder.Build();
+            TestUserName = config["LOGIN.TestUserName"];
+            TestPassword = config["LOGIN.TestPassword"];
+        }
+    }
+
+    public class LoginPageFormTests : PageTest, IClassFixture<LoginPageFixture>
+    {
+        private LoginPage _loginPage;
+        private LoginPageFixture fixture;
+        
+        public LoginPageFormTests(LoginPageFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+        public override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            _loginPage = new LoginPage(Page);
+            await _loginPage.GoTo();
+
+            await Expect(Page).ToHaveTitleAsync(new Regex("Log In"));
+
+            
+        }
+
+        [Fact]
+        public async Task ValidLogin()
+        {
+            await _loginPage.FillLoginFields(fixture.TestUserName, fixture.TestPassword);
+            await _loginPage.ClickLogInButton();
+
+            await Expect(Page).ToHaveURLAsync("https://soundspruce.com/");
+        }
+    }
+}
---
+++ Models/LoginPage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace Playwright_SoundSpruce.Models
+{
+    public class LoginPage
+    {
+        private readonly IPage _page;
+        private readonly ILocator _userName;
+        private readonly ILocator _password;
+        private readonly ILocator _logIn;
+        private readonly ILocator _createAccount;
+        private readonly ILocator _lostPassword;
+        public LoginPage(IPage page)
+        {
+            _page = page;
+            _userName = page.GetByLabel("Username");
+            _password = page.GetByLabel("Password");
+            _logIn = page.GetByRole(AriaRole.Button, new() { Name = "Log in" });
+            _createAccount = page.GetByRole(AriaRole.Link, new() { Name = "Create an account" });
+            _lostPassword = page.GetByRole(AriaRole.Link, new() { Name = "Lost password?" });
+        }
+
+        public async Task GoTo()
+        {
+            await _page.GotoAsync("https://soundspruce.com/register/login/");
+        }
+
+        public async Task FillLoginFields(string userName, string password)
+        {
+            await _userName.FillAsync(userName);
+            await _password.FillAsync(password);
+        }
+
+        public async Task ClickLogInButton()
+        {
+            await _logIn.ClickAsync();
+        }
+
+        public async Task ClickCreateAccount()
+        {
+            await _createAccount.ClickAsync();
+        }
+
+        public async Task ClickLostPassword()
+        {
+            await _lostPassword.ClickAsync();
+        }
+    }
+}
