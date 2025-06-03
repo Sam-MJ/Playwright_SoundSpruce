3e881ca6196250888c8a3fef35d3ecdfd87ae72d
Sam-MJ <tf2redspark@gmail.com>
2025-06-03 12:39:35 +0100

    Added ProductBasePage and SausageFileConverterProductPage

---
+++ Models/ProductBasePage.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Playwright;
+
+namespace Playwright_SoundSpruce.Models
+{
+    public abstract class ProductBasePage : BasePage
+    {
+        protected virtual string ShopUrlSegment => "shop/";
+        private readonly ILocator _buyButton;
+
+        public ProductBasePage(IPage page) : base(page) 
+        {
+            _buyButton = page.GetByRole(AriaRole.Button, new() { Name = "Buy Now" });
+        }
+
+        public async Task ClickBuyButton()
+        {
+            await _buyButton.ClickAsync();
+        }
+    }
+}
---
+++ Models/SausageFileConverterProductPage.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Playwright;
+
+namespace Playwright_SoundSpruce.Models
+{
+    public class SausageFileConverterProductPage : ProductBasePage
+    {
+        private string productSlug = "sausage-file-converter";
+        public override string PageUrl => BaseUrl + ShopUrlSegment + productSlug;
+
+        public SausageFileConverterProductPage(IPage page) : base(page)
+        {
+
+        }
+    }
+}
